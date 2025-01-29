c89709a6d29f94fa70eed1e7d94736f4215149ca
Emma <marmadilemanteater@proton.me>
2025-01-29 04:04:39 -0500

    Move around properties on MainActivity

--- android/app/src/main/java/io/freetubeapp/freetube/MainActivity.kt
+++ android/app/src/main/java/io/freetubeapp/freetube/MainActivity.kt
@@ -47,45 +47,58 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var binding: ActivityMainBinding
-  private lateinit var activityResultListeners: MutableList<(ActivityResult?) -> Unit>
+  // region Keep Alive service
   private lateinit var keepAliveService: KeepAliveService
   private lateinit var keepAliveIntent: Intent
-  private var fullscreenView: View? = null
+  // endregion
+
+  // region JS interfaces
+  private lateinit var jsInterface: FreeTubeJavaScriptInterface
+  lateinit var bgJsInterface: BotGuardJavascriptInterface
+  // endregion
+
+  // region Bindings
+  private lateinit var binding: ActivityMainBinding
   lateinit var webView: BackgroundPlayWebView
   lateinit var bgWebView: BotGuardWebView
-  lateinit var jsInterface: FreeTubeJavaScriptInterface
-  lateinit var bgJsInterface: BotGuardJavascriptInterface
-  private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
   lateinit var content: View
+  private var fullscreenView: View? = null
+  // endregion
+
+  // region Callbacks
+  private lateinit var activityResultListeners: MutableList<(ActivityResult?) -> Unit>
+  private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+  // endregion
+
+  // region State Information
   var consoleMessages: MutableList<JSONObject> = mutableListOf()
   var showSplashScreen: Boolean = true
   var darkMode: Boolean = false
   var paused: Boolean = false
   var isInAPrompt: Boolean = false
+  // endregion
+
+  // region Thread Pool Executor
   /*
    * Gets the number of available cores
    * (not always the same as the maximum number of cores)
    */
-  private val NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors()
-
+  private val numberOfCores = Runtime.getRuntime().availableProcessors()
   // Instantiates the queue of Runnables as a LinkedBlockingQueue
   private val workQueue: BlockingQueue<Runnable> = LinkedBlockingQueue()
-
   // Sets the amount of time an idle thread waits before terminating
-  private val KEEP_ALIVE_TIME = 1
-
+  private val keepAliveTime = 1
   // Sets the Time Unit to seconds
-  private val KEEP_ALIVE_TIME_UNIT: TimeUnit = TimeUnit.SECONDS
-
+  private val keepAliveTimeUnit: TimeUnit = TimeUnit.SECONDS
   // Creates a thread pool manager
   var threadPoolExecutor = ThreadPoolExecutor(
-    NUMBER_OF_CORES,  // Initial pool size
-    NUMBER_OF_CORES,  // Max pool size
-    KEEP_ALIVE_TIME.toLong(),
-    KEEP_ALIVE_TIME_UNIT,
+    numberOfCores,  // Initial pool size
+    numberOfCores,  // Max pool size
+    keepAliveTime.toLong(),
+    keepAliveTimeUnit,
     workQueue
   )
+  // endregion
 
   // region Overridden methods
 
